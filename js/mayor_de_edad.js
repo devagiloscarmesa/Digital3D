2ca563df8d01ca56b859db36028fb3ddc7e15ee1
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-04-24 12:44:27 -0500

    cambios

--- js/mayor_de_edad.js
+++ js/mayor_de_edad.js
@@ -1,8 +1,20 @@
-let edad = prompt("ingrese su edad"); 
-let nombre = prompt("ingrese su nombre");
+function calcularEdad(){
+    let edad = parseInt(document.getElementById("edad").value); 
+    let nombre = document.getElementById("nombre").value;
 
-if(edad>=18 ) {
-    console.log(nombre + " usted es mayor de edad"); 
-} else {
- console.log (nombre + " usted es menor de edad");
+    if(Number.isInteger(edad)){
+        if(edad >= 18) {
+            document.getElementById("mensaje").innerHTML = nombre + " usted es mayor de edad";
+        } else {
+            document.getElementById("mensaje").innerHTML = nombre + " usted es menor de edad";
+        }
+    }else{
+        console.log("La edad no es numerica");
+    }
+}
+
+function calcularIVA(valor){
+    let resultado;
+    resultado = valor * 0.16;
+    return resultado;
 }
