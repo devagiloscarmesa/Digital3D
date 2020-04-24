2a208948f847684c8fcd1ce6b6d38c6d05d2a5ca
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-04-24 13:33:30 -0500

    cambios

--- js/mayor_de_edad.js
+++ js/mayor_de_edad.js
@@ -1,8 +1,13 @@
+let ArrayEdades = new Array();
+	
 function calcularEdad(){
     let edad = parseInt(document.getElementById("edad").value); 
     let nombre = document.getElementById("nombre").value;
-
+    
+    let registro;
     if(Number.isInteger(edad)){
+        registro = {'nombre' : nombre, 'edad' : edad};
+        ArrayEdades.push(registro);
         if(edad >= 18) {
             document.getElementById("mensaje").innerHTML = nombre + " usted es mayor de edad";
         } else {
@@ -17,4 +22,22 @@ function calcularIVA(valor){
     let resultado;
     resultado = valor * 0.16;
     return resultado;
+}
+
+function imprimirPares(){
+    let contador = 0;
+    while(contador <= 20){
+        if((contador % 2) == 0){ 
+            console.log(contador);
+        }
+        contador++;
+    }
+    return false;
+}
+
+function imprimirImpares(){
+	let contador;
+    for(contador = 1 ; contador <= 20 ; contador += 2){
+        console.log(contador)
+    }
 }
