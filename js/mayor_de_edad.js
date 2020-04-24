e5c0e830d5bf5f122245e0cb1cb2aca8778df3ac
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-04-24 11:41:58 -0500

    cambios sass y js

---
+++ js/mayor_de_edad.js
@@ -0,0 +1,8 @@
+let edad = prompt("ingrese su edad"); 
+let nombre = prompt("ingrese su nombre");
+
+if(edad>=18 ) {
+    console.log(nombre + " usted es mayor de edad"); 
+} else {
+ console.log (nombre + " usted es menor de edad");
+}
